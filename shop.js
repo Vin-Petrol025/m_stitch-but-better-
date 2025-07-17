595e2b9de3c28e533b4ca48bae397b50cfdd6698
abdurrahman <a.r.moazzam6171@gmail.com>
2025-07-18 01:08:25 +0500

    changed the brand from m_stitch to belle tasche, added the products, footer, nav, shop.html and all the desired things....

--- shop.js
+++ shop.js
@@ -2,13 +2,29 @@ const productList = document.getElementById("product-list");
 products.forEach((product) => {
   const card = document.createElement("div");
   card.className = "product-card";
-  card.innerHTML = `
-    <a href="product.html?id=${product.id}">
-      <img src="${product.image}" alt="${product.name}" />
+
+  if (product.inStock === 0) {
+    card.innerHTML = `
+      <div class="img-wrapper out-of-stock">
+        <img src="${product.image}" alt="${product.name}" />
+        <span class="sold-out-overlay">SOLD OUT</span>
+      </div>
       <h3>${product.name}</h3>
-      <p class="product-description">${product.description}</p>
+      <p class="product-description">${product.shortDescription}</p>
       <p>${product.price}</p>
-    </a>
-  `;
+    `;
+  } else {
+    card.innerHTML = `
+      <a href="product.html?id=${product.id}">
+        <div class="img-wrapper">
+          <img src="${product.image}" alt="${product.name}" />
+        </div>
+        <h3>${product.name}</h3>
+        <p class="product-description">${product.shortDescription}</p>
+        <p>${product.price}</p>
+      </a>
+    `;
+  }
+
   productList.appendChild(card);
 });
