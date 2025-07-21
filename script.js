69a1197318c4d37ae84c4fd09ba2ed6da24b4a2b
abdurrahman <a.r.moazzam6171@gmail.com>
2025-07-21 23:59:30 +0500

    loading removed.......

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
- window.addEventListener("DOMContentLoaded", () => {
-     const loader = document.getElementById("loader");
 
-     setTimeout(() => {
-         loader.style.opacity = "0";
-         loader.style.transition = "opacity 0.5s ease";
-         setTimeout(() => {
-             loader.style.display = "none";
-         }, 500);
-     }, 100); // 5000 ms = 5 seconds
- });
--- shop.js
+++ shop.js
@@ -1,14 +1,4 @@
- window.addEventListener("DOMContentLoaded", () => {
-   const loader = document.getElementById("loader");
 
-   setTimeout(() => {
-     loader.style.opacity = "0";
-     loader.style.transition = "opacity 0.5s ease";
-     setTimeout(() => {
-       loader.style.display = "none";
-     }, 500);
-   }, 100); // 5000 ms = 5 seconds
- });
 
 
 
