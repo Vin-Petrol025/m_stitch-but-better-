3a549ea82be052670d822257b13353d1192e3a37
abdurrahman <a.r.moazzam6171@gmail.com>
2025-07-21 20:52:19 +0500

    css, js optimizatongit push -u origin maingit push -u origin main

--- shop.js
+++ shop.js
@@ -1,38 +1,14 @@
+ window.addEventListener("DOMContentLoaded", () => {
+   const loader = document.getElementById("loader");
 
-document.addEventListener("DOMContentLoaded", function () {
-  const loader = document.getElementById("loader");
-  const images = document.images;
-  let loadedCount = 0;
-  const totalImages = images.length;
-
-  if (totalImages === 0) {
-    hideLoader();
-  }
-
-  function imageLoaded() {
-    loadedCount++;
-    if (loadedCount === totalImages) {
-      hideLoader();
-    }
-  }
-
-  function hideLoader() {
-    loader.style.opacity = "0";
-    loader.style.transition = "opacity 0.5s ease";
-    setTimeout(() => {
-      loader.style.display = "none";
-    }, 500);
-  }
-
-  for (let i = 0; i < totalImages; i++) {
-    if (images[i].complete) {
-      imageLoaded();
-    } else {
-      images[i].addEventListener("load", imageLoaded);
-      images[i].addEventListener("error", imageLoaded);
-    }
-  }
-});
+   setTimeout(() => {
+     loader.style.opacity = "0";
+     loader.style.transition = "opacity 0.5s ease";
+     setTimeout(() => {
+       loader.style.display = "none";
+     }, 500);
+   }, 5000); // 5000 ms = 5 seconds
+ });
 
 
 
